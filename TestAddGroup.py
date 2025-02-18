9c1d924ee6477af75a83bbd02f337adb6a14af52
world <word.raysz99.games@gmail.com>
2025-02-18 14:52:00 +0600

    test_add_group1.py

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
