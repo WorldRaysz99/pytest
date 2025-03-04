50cd2abb9dc7f42defb4211e818c87b323f3574e
world <word.raysz99.games@gmail.com>
2025-03-04 16:39:51 +0600

    test_add_group1.py

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -7,82 +7,27 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import time, unittest
+
+import pytest
+from application import Application
 from group import Group
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_group_page(wd)
-        self.creation_group(wd, Group("qwer1", "qwer2", "qwer3"))
-        self.return_to_group_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_group_page(wd)
-        self.creation_group(wd, Group("", "", ""))
-        self.return_to_group_page(wd)
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.creation_group(Group("qwer1", "qwer2", "qwer3"))
+    app.logout()
 
-    def login(self, wd, username, password):
-        wd = self.wd
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
-    def open_group_page(self, wd):
-        # open group page
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.creation_group(Group("", "", ""))
+    app.logout()
 
-    def creation_group(self, wd, group):
-        wd = self.wd
-        #init group creation
-        wd.find_element(By.NAME, "new").click()
-        #fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        #submit group creation
-        wd.find_element(By.NAME, "submit").click()
-
-    def return_to_group_page(self, wd):
-        # return to group page
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def logout(self, wd):
-        # logout
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-    
-    def tearDown(self):
-        wd = self.wd
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
